9705d67bfb8da1e00205de14fc6b3c4a37839084
esuye <esubalewbitew13@gmail.com>
2024-01-29 12:26:33 +0300

    created index.tsx for expose image

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Images = {
+    fallBackImage: require('../images/defaultImage.jpg'),
+    backArrow: require('../images/back-arrow.png'),
+    addUser: require('../images/add-user.png')
+};
+
+export default Images;
